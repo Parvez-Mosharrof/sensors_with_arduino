fdb685de329df35f22340a4ba6e13e40671f5202
Parvez Mosharrof <parvezmosharrof0859@gmail.com>
2025-09-28 12:15:32 +0600

    Update sim900a.ino

--- SIM900A_With_Arduino/sim900a.ino
+++ SIM900A_With_Arduino/sim900a.ino
@@ -55,7 +55,7 @@ void SendMessage()
 {
   mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
   delay(1000);  // Delay of 1000 milli seconds or 1 second
-  mySerial.println("AT+CMGS=\"+60XXXXXXXXX\"\r"); // Replace x with mobile number
+  mySerial.println("AT+CMGS=\"+880XXXXXXXXX\"\r"); // Replace x with mobile number
   delay(1000);
   mySerial.println("sim900a sms");// The SMS text you want to send
   delay(100);
